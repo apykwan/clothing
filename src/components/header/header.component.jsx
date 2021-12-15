5a0027706a4c7016244bd2a6652f629cbb9ae2bb
apykwan <apykwan@hotmail.com>
2021-12-15 09:28:06 -0800

    implemented firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,6 @@ import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => {
-    console.log(currentUser);
     return (
         <div className="header">
             <Link className="logo-container" to="/">
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   appId: "1:221401598653:web:d7c634ded3245d9f0885d4"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; 
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+} 
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
